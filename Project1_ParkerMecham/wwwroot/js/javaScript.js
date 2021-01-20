68f74bd4fcea1fdaef06a1bbac17e9f3119a9ab0
Parkmech <mechamparker@gmail.com>
2021-01-20 15:30:54 -0700

    Initial check-in of module Project1_ParkerMecham

---
+++ Project1_ParkerMecham/wwwroot/js/javaScript.js
@@ -0,0 +1,14 @@
+﻿function gradeCalculator()
+{
+    var iAssignments = document.getElementById("assignments").value * .5;
+    var iGroupProject = document.getElementById("groupProjects").value * .1;
+    var iQuizzes = document.getElementById("quizzes").value * .1;
+    var iExams = document.getElementById("exams").value * .2;
+    var iIntex = document.getElementById("intex").value * .1;
+
+    var finalGrade = iAssignments + iGroupProject + iQuizzes + iExams + iIntex;
+
+    var sOutput = "Your final grade in the class is " + finalGrade + "%. Hope you're okay with that.";
+
+    document.getElementById("gradeOutput").innerHTML = sOutput;
+}
