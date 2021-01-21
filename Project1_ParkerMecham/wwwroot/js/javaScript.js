f4b6744e4ec4da089a4939e6b1533c9e1fd0019b
Parkmech <mechamparker@gmail.com>
2021-01-20 17:22:27 -0700

    Updated JavaScript to include letter grades

--- Project1_ParkerMecham/wwwroot/js/javaScript.js
+++ Project1_ParkerMecham/wwwroot/js/javaScript.js
@@ -7,8 +7,46 @@
     var iIntex = document.getElementById("intex").value * .1;
 
     var finalGrade = iAssignments + iGroupProject + iQuizzes + iExams + iIntex;
+    var letterGrade = "";
 
-    var sOutput = "Your final grade in the class is " + finalGrade + "%. Hope you're okay with that.";
+    if (finalGrade < 60) {
+        letterGrade = "E";
+    }
+    if (finalGrade < 64 ) {
+        letterGrade = "D-";
+    }
+    if (finalGrade < 67) {
+        letterGrade = "D";
+    }
+    if (finalGrade < 70) {
+        letterGrade = "D+";
+    }
+    if (finalGrade < 74) {
+        letterGrade = "C-";
+    }
+    if (finalGrade < 77) {
+        letterGrade = "C";
+    }
+    if (finalGrade < 80) {
+        letterGrade = "C+";
+    }
+    if (finalGrade < 84) {
+        letterGrade = "B-";
+    }
+    if (finalGrade < 87) {
+        letterGrade = "B";
+    }
+    if (finalGrade < 90) {
+        letterGrade = "B+";
+    }
+    if (finalGrade < 94) {
+        letterGrade = "A-";
+    }
+    if (finalGrade >= 94) {
+            letterGrade = "A";
+    }
+    
+    var sOutput = "Your final grade in the class is " + finalGrade + "%. That's a(n) " + letterGrade + ". Hope you're okay with that.";
 
     document.getElementById("gradeOutput").innerHTML = sOutput;
 }
